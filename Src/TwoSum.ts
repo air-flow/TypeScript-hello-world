c60ce9ee804942cf75eacf2e7f22d929ff11e1f1
updraft <updraft.1970@gmail.com>
2021-11-05 07:53:29 +0900

    success

--- Src/TwoSum.ts
+++ Src/TwoSum.ts
@@ -1,24 +1,25 @@
 
 
-function twoSum(num: number[], target: number) {
+function twoSum(nums: number[], target: number) {
     let number = []
-    loop :for (let index = 0; index < num.length; index++) {
+    loop :for (let index = 0; index < nums.length; index++) {
         number  =  [index]
-        for (let j = 0; j < num.length; j++) {
+        for (let j = 0; j < nums.length; j++) {
             if (j != index){
-                let temp = [num[number[0]],num[j]]
+                let temp = [nums[number[0]],nums[j]]
                 let total = temp.reduce(function(sum, element){
                     return sum + element;
                 }, 0);
                 if (total == target) {
                     number.push(j)
-                    console.log(number);
+                    // console.log(number);
+                    return number
                     break loop
                 }
             }
         }
     }
-    return []
+    // return []
 }
 let num = [2,7,11,15], target = 9
 console.log(twoSum(num,target));
