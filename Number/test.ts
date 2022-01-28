206f3ede47470d06de71807f8d85af55e28712b7
updraft <updraft.1970@gmail.com>
2022-01-28 10:35:49 +0900

    test

---
+++ Number/test.ts
@@ -0,0 +1,29 @@
+import { Interface } from "readline";
+
+let a: Number
+let b: number = 1;
+
+a = 1
+b = 1
+console.log(a);
+console.log(b);
+
+function numberTest(n: number) {
+    console.log("number function");
+}
+
+function NumberTest(n: Number) {
+    console.log("Number function");
+}
+console.log(Number.NaN);
+
+// numberTest(a)
+// NumberTest(a)
+
+// numberTest(b)
+// NumberTest(b)
+
+
+Interface Number2{
+
+}
